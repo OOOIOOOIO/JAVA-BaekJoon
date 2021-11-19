acc96611e58141c8ffd9326d6a35f5824a773c49
Seongho Kim <74396651+OOOIOOOIO@users.noreply.github.com>
2021-11-20 01:18:44 +0900

    commit

---
+++ 스택/균형잡힌세상_4949.java
@@ -0,0 +1,61 @@
+package ½ºÅÃ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class ±ÕÇüÀâÈù¼¼»ó_4949 {
+	
+	public String match(String input) {
+		Stack<Character> stack = new Stack();
+		
+		for(char a : input.toCharArray()) {
+			if(a == '(' || a == '[') {
+				stack.push(a);
+			}
+			
+			
+			else if(a == ')') {
+				if(!(stack.empty()) && stack.peek() == '(') stack.pop();
+                    
+                else return "no";
+				
+			}
+			
+			else if(a == ']') {
+				if(!(stack.empty()) && stack.peek() == '[') stack.pop();
+                    
+                else return "no";
+			}
+		}
+		if(stack.isEmpty()) {
+			return "yes";
+		}
+		else {
+			return "no";
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		±ÕÇüÀâÈù¼¼»ó_4949 test = new ±ÕÇüÀâÈù¼¼»ó_4949();
+		
+		
+		while(true) {
+			String input = br.readLine();
+			if(input.equals(".")) break;
+			
+			
+			sb.append(test.match(input)).append("\n");
+			
+			
+		}
+		
+		System.out.println(sb);
+	}
+}
