5cde982f9d3a6274dcab89b6b0cc42172b8e54cf
Seongho Kim <74396651+OOOIOOOIO@users.noreply.github.com>
2021-11-25 15:54:02 +0900

    commit

---
+++ 큐/요세푸스_11866.java
@@ -0,0 +1,38 @@
+package Å¥_µ¦;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class ¿ä¼¼Çª½º_11866 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Deque<Integer> dq = new ArrayDeque<Integer>();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int r = K;
+		for(int i = 1; i <= N; i++) {
+			dq.addLast(i);
+		}
+
+		sb.append("<");
+		//(7,3)
+		for(int i = 0; i < N -1; i++) {
+			for(int j = 0; j < K-1; j++) {
+				dq.addLast(dq.remove());
+			}
+			sb.append(dq.remove()).append(", ");
+		}
+		sb.append(dq.peek()).append(">");
+		
+		System.out.println(sb);
+		
+
+	}
+}
---
+++ 큐/카드2_2164_.java
@@ -0,0 +1,28 @@
+package Å¥_µ¦;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Ä«µå2_2164_ {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Deque<Integer> dq = new ArrayDeque<Integer>();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= N; i++) {
+			dq.addLast(i);
+		}
+		
+		while(true) {
+			if(dq.size() == 1) break;
+			dq.removeFirst();
+			dq.addLast(dq.removeFirst());
+		}
+//		System.out.println(dq.size());
+		System.out.println(dq.peekFirst());
+	}
+}
