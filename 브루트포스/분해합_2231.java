0c3e2903e53ca33c1c14a143d207720ec23c9364
Seongho Kim <74396651+OOOIOOOIO@users.noreply.github.com>
2022-02-24 12:51:59 +0900

    .

--- 브루트포스/분해합_2231.java
+++ 브루트포스/분해합_2231.java
@@ -1,8 +1,8 @@
-package ºê·çÆ®Æ÷½º;
+package ë¸Œë£¨íŠ¸í¬ìŠ¤;
 
 import java.util.Scanner;
 
-public class ºĞÇØÇÕ_2231 {
+public class ë¶„í•´í•©_2231 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
