17c60294c3c7e60a10a04b42c51906f545a5d289
Seongho Kim <74396651+OOOIOOOIO@users.noreply.github.com>
2022-02-26 23:35:43 +0900

    .

--- Math/소수_2581.java
+++ Math/소수_2581.java
@@ -1,11 +1,11 @@
-package ¼öÇÐ;
+package ìˆ˜í•™;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class ¼Ò¼ö_2581 {
+public class ì†Œìˆ˜_2581 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] arr = new int[100001];
