747c63a520a44cf7907370a91da58ed92356c946
Seongho Kim <74396651+OOOIOOOIO@users.noreply.github.com>
2022-02-26 23:36:13 +0900

    .

--- Math/손익분기점_1712.java
+++ Math/손익분기점_1712.java
@@ -1,8 +1,8 @@
-package ¼öÇĞ;
+package ìˆ˜í•™;
 
 import java.util.Scanner;
 
-public class ¼ÕÀÍºĞ±âÁ¡_1712 {
+public class ì†ìµë¶„ê¸°ì _1712 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int A = sc.nextInt();
